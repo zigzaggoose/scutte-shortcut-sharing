0401c40179a567fa9093594ac6be1a358d32d18d
zigzaggoose <rishab9001@gmail.com>
2025-12-31 15:58:59 +0400

    Initial Commit

---
+++ shortcut hub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  shortcut hub
+//
+//  Created by Rishab Reji on 31/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ shortcut hub/shortcut_hubApp.swift
@@ -0,0 +1,17 @@
+//
+//  shortcut_hubApp.swift
+//  shortcut hub
+//
+//  Created by Rishab Reji on 31/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct shortcut_hubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
